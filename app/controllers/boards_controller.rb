8c931d9b4c6678ca0ea3e8535c4cbf03e6cf4370
takumi <takumim1005@gmail.com>
2023-04-13 18:10:47 +0900

    データ作成処理

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -5,4 +5,13 @@ class BoardsController < ApplicationController
 	def new
 		@board = Board.new
 	end
+
+	def create
+		Board.create(board_params)
+	end
+
+	private 
+		def board_params
+		params.require(:board).permit(:name, :title, :body)
+		end
 end
