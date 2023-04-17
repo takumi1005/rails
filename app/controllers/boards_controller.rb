7ecf90ed6dcb117dd805606d50dd88ad9394a55a
takumi <takumim1005@gmail.com>
2023-04-17 18:13:11 +0900

    before_actionで処理を共通化

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,4 +1,7 @@
 class BoardsController < ApplicationController
+
+	before_action :set_target_board, only: %i[show edit update destroy]
+
 	def index
 		@boards = Board.all
 	end
@@ -13,29 +16,30 @@ class BoardsController < ApplicationController
 	end
 
 	def show
-		@board = Board.find(params[:id])
 	end
 
 	def edit
-		@board = Board.find(params[:id])
 	end
 
 	def update
-		board = Board.find(params[:id])
-		board.update(board_params)
+		@board.update(board_params)
 
-		redirect_to board
+		redirect_to @board
 	end
 
 	def destroy
-		board = Board.find(params[:id])
-		board.delete
+		@board.delete
 
 		redirect_to boards_path
 	end
 
-	private 
-		def board_params
+	private
+
+	def board_params
 		params.require(:board).permit(:name, :title, :body)
-		end
+	end
+
+	def set_target_board
+		@board = Board.find(params[:id])
+	end
 end
